5f5518cf25c69094211f76b74dfa628c71df11a9
thompsonseanr <thompson.sean.r@gmail.com>
2026-09-19 15:29:58 -0700

    dev: beginning with var declarations.

---
+++ py_displaylink_synaptics_install/py_displaylink_synaptics_install.py
@@ -0,0 +1,61 @@
+import os
+import subprocess
+from pathlib import Path
+
+# Personal Dev Notes: 
+# Will move playwright_scraper.py and executable over.
+# This will require either including the playwright_scraper.py itself or the code inside as a function.
+# Playwright drivers will have to be installed with this ENV:
+# PLAYWRIGHT_BROWSERS_PATH=0 playwright install chromium
+# 
+# Python f-string example:  
+# command = f'ls -l "{VAR}"'  
+#
+# Python subprocess.run output: $VAR.stdout
+
+def dir_find(initSearchDir: str, targetObj: str) -> list[Path]:
+    return [d for d in Path(initSearchDir).rglob(targetObj) if d.is_dir()]
+
+def file_find(initSearchDir: str, targetObj: str) -> list[Path]:
+    return [f for f in Path(initSearchDir).rglob(targetObj) if f.is_file()]
+
+locUser: str | None = os.getenv('USER')
+runitTest: subprocess.CompletedProcess[str] = subprocess.run("ps -p 1 -o comm=", shell=True, capture_output=True, text=True)
+
+initSearchDir: str = f"/home/{locUser}"
+evdiRepo: str = "https://github.com/DisplayLink/evdi.git"
+evdiDirFind: list[Path] = dir_find(initSearchDir, "evdi")
+evdiGitPath: Path | None = evdiDirFind[0] if evdiDirFind else None
+evdiTarPath: str | None = os.path.dirname(evdiGitPath) if evdiGitPath else None
+evdiGitMain: str = ""
+evdiGitTag: str = ""
+
+# Current DisplayLink Download: https://www.synaptics.com/sites/default/files/exe_files/2026-06/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.3-EXE.zip
+displayLinkDl: str = ""
+displayLinkScraperFind: list[Path] = file_find(initSearchDir, "playwright_scraper")
+displayLinkScraper: Path | None = displayLinkScraperFind[0] if displayLinkScraperFind else None
+for scraper in displayLinkScraperFind:
+    print(f"scraper: {scraper}")
+displayStatus: str = ""
+displayLinkFullName: str = ""
+displayLinkPath: str = ""
+displayLinkName: str = ""
+displayLinkNameFix: str = ""
+displayLinkVer: str = ""
+displayLinkTarget: str = ""
+displayLinkFileDir: str = ""
+displayLinkInstallDir: str = ""
+dispArr: list[Path] = file_find(initSearchDir, "DisplayLink*.zip")
+dispArrVal: Path | None = dispArr[0] if dispArr else None
+for displayVal in dispArr:
+    print(f"displayVal: {displayVal}")
+print(dispArrVal)
+
+evdiTest: subprocess.CompletedProcess[str] = subprocess.run(f'lsmod | grep -Eio "evdi" | head -1', shell=True, capture_output=True, text=True)
+displayInstallerTest: str = "/usr/bin/displaylink-installer"
+installDec: str = ""
+print(f"lsmod: {evdiTest.stdout}")
+
+downloadEvdiFile: str = "false"
+downloadDisplayFile: str = "false"
+
