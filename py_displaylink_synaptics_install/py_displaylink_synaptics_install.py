7ddef6b6738a8e99130ecb344f016e41b3d20160
thompsonseanr <thompson.sean.r@gmail.com>
2026-09-20 23:24:59 -0700

    dev: continued python dev. Converted clean_files() method.

--- py_displaylink_synaptics_install/py_displaylink_synaptics_install.py
+++ py_displaylink_synaptics_install/py_displaylink_synaptics_install.py
@@ -1,8 +1,11 @@
 import os
+import sys
 import subprocess
+import shutil
 from pathlib import Path
 from textual import events
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.widgets import Header, Footer, RichLog, Welcome, Label, Button
 
 # Personal Dev Notes: 
@@ -40,46 +43,74 @@ displayLinkScraper: Path | None = displayLinkScraperFind[0] if displayLinkScrape
 for scraper in displayLinkScraperFind:
     print(f"scraper: {scraper}")
 displayStatus: str | None = ""
-displayLinkFullName: str | None = ""
-displayLinkPath: str | None = ""
-displayLinkName: str | None = ""
-displayLinkNameFix: str | None = ""
-displayLinkVer: str | None = ""
-displayLinkTarget: str | None = ""
-displayLinkFileDir: str | None = ""
-displayLinkInstallDir: str | None = ""
+displayLinkFullNameFind: list[Path] = file_find(initSearchDir, "DisplayLink*.zip")
+displayLinkFullName: Path | None = displayLinkFullNameFind[0] if displayLinkScraperFind else None
+displayLinkPath: str | None = None
+displayLinkName: str | None = None
+displayLinkNameFix: str | None = None
+displayLinkVer: str | None = None
+displayLinkTarget: str | None = None
+displayLinkFileDir: Path | None = None
+displayLinkInstallDir: Path | None = None
 dispArr: list[Path] = file_find(initSearchDir, "DisplayLink*.zip")
 dispArrVal: Path | None = dispArr[0] if dispArr else None
 for displayVal in dispArr:
     print(f"displayVal: {displayVal}")
 print(dispArrVal)
+# Logic if dispArr
+# Refactor and rethink
 
 evdiTest: subprocess.CompletedProcess[str] = subprocess.run(f'lsmod | grep -Eio "evdi" | head -1', shell=True, capture_output=True, text=True)
-displayInstallerTest: str = "/usr/bin/displaylink-installer"
+displayInstallerTest: Path = Path("/usr/bin/displaylink-installer")
 installDec: str = ""
 print(f"lsmod: {evdiTest.stdout}")
 
-downloadEvdiFile: str = "false"
-downloadDisplayFile: str = "false"
+downloadEvdiFile: bool = False
+downloadDisplayFile: bool = False
+
+# Test for DisplayLink
+isDisplayLinkInstalled: bool = True if (evdiTest.stdout and displayInstallerTest.is_file()) else False
+
+print(f"isDisplayLinkInstalled: {isDisplayLinkInstalled}")
+
+def clean_files() -> None:
+    if downloadEvdiFile:
+        if Path(f"{evdiTarPath}/evdi.tar.gz").is_file():
+            os.remove(f"{evdiTarPath}/evdi.tar.gz")
+        if evdiGitPath:
+            shutil.rmtree(evdiGitPath)
+    if downloadDisplayFile:
+        if displayLinkFullName:
+            os.remove(displayLinkFullName)
+        if displayLinkInstallDir and displayLinkInstallDir.is_dir():
+            shutil.rmtree(displayLinkInstallDir)
+    sys.exit(1)
 
 
 
 
-class QuestionApp(App[str]):
+
+
+class DisplayLinkInstall(App):
+    BINDINGS = [
+        Binding(key="q", action="quit", description="Quit the DisplayLink Installer")
+    ]
     def on_mount(self) -> None:
         self.theme = "nord"
     def compose(self) -> ComposeResult:
+        yield Header()
         yield Label(":::::::::::::::::::::::::::::::::::::::::::")
         yield Label("::: DisplayLink Installer")
         yield Label(":::::::::::::::::::::::::::::::::::::::::::")
-        yield Button("Yes", id="yes", variant="primary") 
-        yield Button("No", id="no", variant="error")
+        yield Button("Start", id="start", variant="primary") 
+        # yield Button("No", id="no", variant="error")
+        yield Footer()
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         self.exit(event.button.id)
 
 if __name__ == "__main__":
-    app = QuestionApp()
+    app = DisplayLinkInstall()
     reply = app.run()
     print(reply)
     # app.run()
